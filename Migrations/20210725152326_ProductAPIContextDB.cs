0025ae4b83c141e7082a64bb88c00ff75e66df1d
rahulshrivastava140387 <rahulshrivastava1501@outlook.com>
2021-07-25 21:58:47 +0530

    Initial commit

---
+++ Migrations/20210725152326_ProductAPIContextDB.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ShopBridgeAPI.Migrations
+{
+    public partial class ProductAPIContextDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Product",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    productName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    productDescription = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    productCategory = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    productPrice = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Product", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Product");
+        }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopBridgeAPI.Models
+{
+    public class Product
+    {
+        [Required, Key]
+        public int ID { get; set; }
+        [MaxLength(50, ErrorMessage = "Product name should not exceed 50 characters.")]
+        public string productName { get; set; }
+        [MaxLength(500, ErrorMessage = "Product description should not exceed 500 characters.")]
+        public string productDescription { get; set; }
+        public string productCategory { get; set; }
+        public double productPrice { get; set; }
+    }
+}
